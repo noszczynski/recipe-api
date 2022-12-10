a613404f7d69523e00e0c1ab877b3a54897e8ff6
adamnoszczynski <adam.noszczynski@gmail.com>
2022-12-10 01:11:14 +0100

    Connect with local postgresql
    create connection with psql and configure envs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { AppController } from './app.controller';
 import { RecipeModule } from '../recipe/recipe.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from '../user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     RecipeModule,
     UserModule,
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: './database/recipe-api.sqlite3',
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       autoLoadEntities: true,
       synchronize: true, // not for prod, use migrations
     }),
